aa8c388f9a1f4a98d584021fa6c6d8f788e93ad2
Tosh <owner@Owners-MacBook-Pro.local>
2019-02-14 22:33:15 +0300

    add var output

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 var triangleType = function(){
   var sides= parseInt(document.getElementById('sides').value);
   var equal= parseInt(document.getElementById('equal').value);
+
+  var output= document.getElementById('output');
 }
