42cb25e4377e85bbb0beb43dd9a92be24eeb1e04
Tosh <owner@Owners-MacBook-Pro.local>
2019-02-14 22:42:59 +0300

    add output.context function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,4 +3,11 @@ var triangleType = function(){
   var equal= parseInt(document.getElementById('equal').value);
 
   var output= document.getElementById('output');
-}
+
+  if(isNaN(sides)|| isNaN(equal)){
+    output.textContent = "Please enter a number!"
+  } else if {
+    sides == 3 && equal == 3;
+    output.textContent=("This is an equilateral triangle")
+  }
+};
