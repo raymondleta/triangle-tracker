4b9407c33be246fcaf53e3a8ca76c93d71d9b2cc
System Administrator <root@Owners-MacBook-Pro.local>
2019-02-15 15:02:31 +0300

    add font style to h1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,17 @@
 function stoppedTyping(){
-        if(this.length < 0) {
-            document.getElementById('submitButton').disabled = true;
-        } else {
-            document.getElementById('submitButton').disabled = false;
-        }
+    if(this.length < 0) {
+        document.getElementById('submitButton').disabled = true;
+      } else {
+        document.getElementById('submitButton').disabled = false;
+      }
     };
 
 function trackButton(){
   var A=parseFloat(document.getElementById("sideA").value);
   var B=parseFloat(document.getElementById("sideB").value);
   var C=parseFloat(document.getElementById("sideC").value);
-  // console.log(A);
 
-
-    // if((A+B)<=C || (A+C)<=B || (B+C)<=A){
-    //   document.getElementById('answerHere').innerHTML="This is not a triangle";
-    // }else if (A===B!==C || A!==B===C || A!==C===B) {
-    //   document.getElementById('answerHere').innerHTML="This is an isoceles triangle";
-    // }else if (A===B===C || A===C===B) {
-    //   document.getElementById('answerHere').innerHTML="This is an equilateral triangle";
-    // }else if (A!==B!==C || A!==C!==B) {
-    //   document.getElementById('answerHere').innerHTML="This is a scalene triangle";
-    // }else {
-    //   document.getElementById('answerHere').innerHTML="Please enter values";
-    // }
-
-
-    if((A+B)<=C || (B+C)<=A || (C+A)<=B){
+  if((A+B)<=C || (B+C)<=A || (C+A)<=B){
       document.getElementById('answerHere').innerHTML="This is not a triangle";
     }else if ((A===B) && (B===C) && (C===A)){
       document.getElementById('answerHere').innerHTML="This is an equilateral triangle";
