d55bde989e5d5a1c5bdcc3405bbd49d01331e8c3
System Administrator <root@Owners-MacBook-Pro.local>
2019-02-15 09:34:51 +0300

    declare var triangle tracker in scripts file and call get ElementByIid function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,5 @@
-
+function trackButton(){
+  var side1=document.getElementById("sideA").value;
+  var side2=document.getElementById("sideB").value;
+  var side3=document.getElementById("sideC").value;
+}
